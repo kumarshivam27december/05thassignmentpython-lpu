2010fc81159926912baa75bf4bae02e06b1f7970
shivamkumar <kumarshivam16062004@gmail.com>
2022-10-16 14:01:36 +0530

    version 1.0

---
+++ boolean.py
@@ -0,0 +1,25 @@
+print(5>2)
+print(5==2)
+print(5<2)
+
+# bool method that evaluates the value and return true or false 
+print(bool('hello'))
+print(bool(20))
+#  note : values are true almost every value 
+# if value has a data it return true. 
+# any string will return true except empty string 
+# any number retuurn true except zero 
+# any list tuple ,sets, dictionary will return true except empty once
+'''   values that are false are respectively
+# print(bool(false))
+# print(bool(false))
+# print(bool(false))
+# print(bool(false))
+# print(bool(false))
+# print(bool(false))   '''
+
+
+
+
+
+
